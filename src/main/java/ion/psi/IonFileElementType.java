2da44d9ad3145630eb55a5039421ee921a963ab6
Dmitry Neverov <dmitry.neverov@jetbrains.com>
2019-12-18 08:32:40 +0100

    stubs 1

--- src/main/java/ion/psi/IonFileElementType.java
+++ src/main/java/ion/psi/IonFileElementType.java
@@ -1,9 +1,10 @@
 package ion.psi;
 
-import com.intellij.psi.tree.IFileElementType;
+import com.intellij.psi.stubs.PsiFileStub;
+import com.intellij.psi.tree.IStubFileElementType;
 import ion.IonLanguage;
 
-public class IonFileElementType extends IFileElementType {
+public class IonFileElementType extends IStubFileElementType<PsiFileStub<?>> {
   public static final IonFileElementType INSTANCE = new IonFileElementType();
 
   public IonFileElementType() {
